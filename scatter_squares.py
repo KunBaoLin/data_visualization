3f8ca6dff8e3769b07180067d9f4da93cd87768e
KunBao(Leo) <88116174+KunBaoLin@users.noreply.github.com>
2021-12-25 21:21:41 -0800

    Add files via upload

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,4 +1,10 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2,4)
+plt.scatter(2,4, s=2000)
+plt.title("square numbers", fontsize =24)
+plt.xlabel('values', fontsize = 14)
+plt.ylabel('square of values', fontsize = 14)
+plt.tick_params(axis='both', which = 'major', labelsize =14)
+
+
 plt.show()
