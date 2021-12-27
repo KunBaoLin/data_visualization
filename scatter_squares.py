476b226f9cc3e3495abb0c12a4c15cf7a80c589f
KunBao(Leo) <88116174+KunBaoLin@users.noreply.github.com>
2021-12-26 22:25:48 -0800

    Add files via upload

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2,4, s=2000)
+x_values = [1,2,3,4,5]
+y_values = [1,4,9,16,25]
+
+
+plt.scatter(x_values,y_values, s=200)
 plt.title("square numbers", fontsize =24)
 plt.xlabel('values', fontsize = 14)
 plt.ylabel('square of values', fontsize = 14)
